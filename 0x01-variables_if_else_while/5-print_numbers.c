917139876f96791af8279cd2fcaa31f22a4457f6
Egberanmwen <doris_esh@yahoo.com>
2023-02-19 11:36:19 -0800

    "5-print_numbers.c and 5-print_number"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * betty style doc for function mainis inserted here
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+/* Your code goes there*/
+for (a = 0; a < 10; a++)
+printf("%d\n", a);
+return (0);
+}
