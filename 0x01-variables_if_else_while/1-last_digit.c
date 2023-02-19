da4663974467a813bbdabbc087cc71ef1bbc0754
Egberanmwen <doris_esh@yahoo.com>
2023-02-18 21:04:21 -0800

    1-last_digit.c and 1-last_digit"

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ n = rand() - RAND_MAX / 2;
 
 m = n % 10;
 if (m > 5)
-printf("the last digit of %d is %d and is greater than 5\n", n, m);
+printf(" last digit of %d is %d and is greater than 5\n", n, m);
 if (m == 0)
-printf("the last digit of %d is %d and is 0\n", n, m);
+printf("last digit of %d is %d and is 0\n", n, m);
 if (m < 6 && m != 0)
-printf("the last digit of %d is %d and is less than 6 and not a 0\n", n, m);
+printf("last digit of %d is %d and is less than 6 and not a 0\n", n, m);
 return (0);
 }
