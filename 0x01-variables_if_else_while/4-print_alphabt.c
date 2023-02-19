3a8b2634377ad30b3d76262aa9f570b34cc956b2
Egberanmwen <doris_esh@yahoo.com>
2023-02-19 06:53:01 -0800

    "4-print_alphabt.c and 4-print_alphabt"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * betty style doc for function main goes there
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+/*Your code goes there*/
+c = 'a';
+while (c <= 'z')
+{putchar(c);
+}
+putchar('\n');
+return (0);
+}
