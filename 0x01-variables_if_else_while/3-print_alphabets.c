ebfe1bb5906a9ed7309848df1f44331ef9ffe110
Egberanmwen <doris_esh@yahoo.com>
2023-02-19 05:54:06 -0800

    "2-print_alphabet.c and 2-print_alphabet"

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /*
- *  main-Entry point
- *  betty style doc for function main goes there
- * return : Always 0 (Success)
+ * main -Entry point
+ * betty doc style for function main goes there
+ * return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
 	char d;
-/* Your code goes there */
+/*Your code goes there*/
+
 c = 'a';
 d = 'A';
 while (c <= 'z')
-{putchar(c), c++;
+{putchar (c), c++;
 }
 while (d <= 'Z')
-{putchar(d), d++;
+{putchar (d), d++
 }
 putchar('\n');
-return (0);
 }
