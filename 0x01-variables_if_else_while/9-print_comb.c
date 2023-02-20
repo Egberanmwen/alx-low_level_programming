6a12460a520861e3c11feb676d29bdcd9bec2305
Egberanmwen <doris_esh@yahoo.com>
2023-02-20 14:18:30 -0800

    "9-print_comb.c and 9-print_comb"

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,11 @@ int main(void)
 	int n;
 
 /*Your code is inserted here*/
-for (n = 48; n < 57; n++)
+for (n = 48; n <= 57; n++)
 {putchar (n);
+if (n == 57)
+{continue;
+}
 putchar (',');
 putchar (' ');
 }
