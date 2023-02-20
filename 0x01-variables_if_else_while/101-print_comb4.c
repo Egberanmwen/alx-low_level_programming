e48cf0ef7a9458e9381213b8214e7d5931e35772
Egberanmwen <doris_esh@yahoo.com>
2023-02-20 15:23:58 -0800

    "101-print_comb4.c and 101-print_comb4"

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * betty style doc for function main is inserted here
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+for (i = 48; i <= 58; i++)
+{
+for (j = 49; j <= 58; j++)
+{
+for (k = 50; k <= 58; k++)
+{
+putchar (i);
+putchar (j);
+putchar (k);
+if (i != 55 || j != 56)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
