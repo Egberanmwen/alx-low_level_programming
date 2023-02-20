3ee3c1aca11f23a84874271c302fdf3c0bb44139
Egberanmwen <doris_esh@yahoo.com>
2023-02-20 13:58:50 -0800

    "9-print_comb.c and ./9-print_comb"

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * betty style doc for function main is inserted here
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+/*Your code is inserted here*/
+for (n = 48; n < 57; n++)
+{putchar (n);
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
