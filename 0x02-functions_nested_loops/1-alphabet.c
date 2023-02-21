52ec26f38ef1e8bd54d0f9058cc0db016c282762
Egberanmwen <doris_esh@yahoo.com>
2023-02-21 14:49:20 -0800

    fix: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * betty style doc for function main is inserted here
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int j;
+/*Your code is inserted here*/
+for (j = 'a'; j <= 'z'; j++)
+_putchar (j);
+_putchar ('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * betty style doc for function main is inserted here
+ * Return: Always 0 (Success_
+ */
+int main(void)
+{
+return (0);
+}
