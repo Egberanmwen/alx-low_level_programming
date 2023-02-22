198b29d9176dfab931f62c427dc6a4baab7be080
Egberanmwen <doris_esh@yahoo.com>
2023-02-22 14:32:39 -0800

    "fix:9-times_table.c"

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,19 +3,19 @@
 *times_table - Prints the 9 times table, starting with 0
 *betty style doc for function main is inserted here
 *Return:rimes table
-*tone = row, cone = column,d = digit of the current result
+*rone = row, cone = column,d = digit of the current result
 */
 void times_table(void)
 {
 	int tone, cone, d;
-for (tone = 0; tone <= 9; tone++)
+for (rone = 0; rone <= 9; rone++)
 {
 _putchar ('0');
 _putchar (',');
 _putchar (' ');
 for (cone = 0; cone <= 9; cone++)
 {
-d = (tone * cone);
+d = (rone * cone);
 if ((d / 10) > 0)
 {
 _putchar ((d / 10) + '0');
