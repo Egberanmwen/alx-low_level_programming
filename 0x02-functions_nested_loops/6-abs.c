238e807b448def0938d778f8e1db68a316049cc8
Egberanmwen <doris_esh@yahoo.com>
2023-02-22 13:18:39 -0800

    fix:6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * int _abs -computes the absolute value of an integer.
- * Return:the absolute value of int.
- * @n: the int in question.
+ *_abs - Computes the absolute value of an integer.
+ *Return:the absolute value of int.
+ *@n: the int in question.
  *betty style doc for function mainis inserted here.
  */
 int _abs(int n)
