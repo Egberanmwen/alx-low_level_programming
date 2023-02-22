f55ca97a514b00df61db6f4d8d4b472f348d0737
Egberanmwen <doris_esh@yahoo.com>
2023-02-22 06:48:39 -0800

    "fix:2-print_alphabet_x10.c"

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
+	int count = 0;
 	int j;
 /*Your code is insertde here*/
+while (count++ <= 9)
+{
 for (j = 'a'; j <= 'z'; j++)
 _putchar (j);
 _putchar ('\n');
+}
 }
