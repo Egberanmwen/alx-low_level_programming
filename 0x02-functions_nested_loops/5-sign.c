d001f0a4efb817899904b87d9b6ddd0071d71fbf
Egberanmwen <doris_esh@yahoo.com>
2023-02-22 11:11:05 -0800

    fix:4-isalpha.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_sign -the function that prints the sign of a number.
+ * Return:1 and prints + if n is greater than zero.
+ * Return:0 and prints 0 if n is zero.
+ * Return:-1 and prints - if n is less than zero.
+ * betyy style doc for function is inserted here.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return (0);
+}
