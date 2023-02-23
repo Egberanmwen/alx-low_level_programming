c43aaa97c39a7dae248910a9e52fc219429be1e5
Egberanmwen <doris_esh@yahoo.com>
2023-02-23 03:11:33 -0800

    message:1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isdigit-a function that checks for a digit (0 through 9).
+*@c:input.
+*Return:1 if c is a digit 0therwise 0.
+*/
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0)
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _isupper(int c);
+int _isdigit(int c);
