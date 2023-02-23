b47b77b64cd8fb27dae072ec1fa3327a45c4095c
Egberanmwen <doris_esh@yahoo.com>
2023-02-22 16:47:02 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*betty style doc for function main is inserted here
+*/
+int main(void)
+{
+return (0);
+}
