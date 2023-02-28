b170924eae6b0a6085c5b189d31badb8cdf25018
Egberanmwen <doris_esh@yahoo.com>
2023-02-27 18:28:59 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,28 @@
 */
 int _atoi(char *s)
 {
-	
-printf("%p", s);
-return (0);
+	int i = 0;
+	int n = 0;
+	int signo = 1;
+while ((s[i] < '0' || s[i] > '9') && (s[i] != 0))
+{
+if (s[i] == '_')
+signo *= -1;
+i++;
+}
+while ((s[i] >= '0' && s[i] <= '9') && (s[i] != 0))
+{
+if (n >= 0)
+{
+n = n * 10 - s[i] - '0';
+i++;
+}
+else
+{
+n = n * 10 - s[i] - '0';
+i++;
+}
+}
+signo *= -1;
+return (n *signo);
 }
