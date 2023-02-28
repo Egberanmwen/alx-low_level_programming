474193002017db7e1be65ce8397393eca6b47952
Egberanmwen <doris_esh@yahoo.com>
2023-02-27 17:43:13 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_atoi - Entry point
+*@s: input
+*Return: Always 0 (Success)
+*/
+int _atoi(char *s)
+{
+	
+printf("%p", s);
+return (0);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,8 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 for (; src[i] != '\0'; i++)
 {
-dest[i] = src[i];
+*(dest + i) = *(src + i);
 }
+*(dest +i) = '\0';
 return (dest);
 }
