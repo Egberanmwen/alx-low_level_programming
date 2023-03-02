e0549afbce3d915fda10c75ea60964d69daf5edf
Egberanmwen <doris_esh@yahoo.com>
2023-03-02 12:56:31 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,19 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, co;
-for (i = 0; dest[i] != '\0'; i++)
+	int i, j;
+i = 0;
+while (dest[i] != '\0')
 {
+i++;
 }
-for (co = 0 ; co < n; co++)
+j = 0;
+while (j < n && src[j] != '\0')
 {
-dest[i + n] = src[co];
-if (src[co] == '\0')
-co = n;
+dest[i] = src[j];
+i++;
+j++;
 }
+dest[i] = '\n';
 return (dest);
 }
