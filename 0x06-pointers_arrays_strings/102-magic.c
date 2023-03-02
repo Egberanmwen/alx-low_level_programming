de96bdf466b32731e782387c650e1185a721f3e3
Egberanmwen <doris_esh@yahoo.com>
2023-03-02 13:51:11 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modi* - only one statement
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modi* - only one statement
 * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
