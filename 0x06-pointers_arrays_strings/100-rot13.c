d8ed765b6267fd6c3a97216c17b76617f00915e9
Egberanmwen <doris_esh@yahoo.com>
2023-03-02 13:23:45 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,28 +5,20 @@
 *@n: input
 *Return: decrypted string
 */
-char *rot13(char *n)
+char *rot13(char *s)
 {
 	int i, j;
-	char data[] = {'A', 'B', 'C', 'D', 'E' 'F', 'G', 'H', 'I',
-		'J', 'K', 'M', 'N', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-		'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'i',
-		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-		'w', 'x', 'y', 'z'};
-	char datarot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		'W', 'X'', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-		'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z' 'a', 'b', 'c', 'd', 'e', 'f',
-		'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-for (i = 0; n[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 52; j+++)
-if (n[i] == data[j])
+for (j = 0; j < 52; j++)
+if (s[i] == data[j])
 {
-n[i] = datarot[j];
+s[i] = datarot[j];
 break;
 }
 }
-return (n);
+return (s);
 }
