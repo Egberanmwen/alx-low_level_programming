a60f639ce773da35a07e477a4d7682a8b1dbdbda
Egberanmwen <doris_esh@yahoo.com>
2023-03-07 17:06:10 -0800

    "2-strlen_recursion.c"

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strlen_recursion note: no _putchar in local test
- * @s: input
- * Return: Awlays 0 (Success)
- */
+* _strlen_recursion - note: no _putchar in local test
+* @s: input
+* Return: Awlays 0 (Success)
+*/
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
