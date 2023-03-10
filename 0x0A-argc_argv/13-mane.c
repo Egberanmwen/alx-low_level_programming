5a262b56206b7ae815bad62c9006936607b58a90
Egberanmwen <doris_esh@yahoo.com>
2023-03-10 12:36:14 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/13-mane.c
+++ 0x0A-argc_argv/13-mane.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - prints its name + \n
  * @argc: number of arguments typed
  * @argv: array pointing to arguements
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-argc = 0;
-
-printf("%s\n", argv[argc]);
-return (0);
+        char *name = argv[0];
+printf("Program name: %s\n", name);
+return 0;
 }
