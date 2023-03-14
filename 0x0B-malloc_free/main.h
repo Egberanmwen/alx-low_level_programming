7cbd4773a67644689bd32fe265f9a0f20a2977c9
Egberanmwen <doris_esh@yahoo.com>
2023-03-14 10:50:02 -0700

    100-argstostr.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,4 +5,8 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+char *create_array(unsigned int size, char c);
+char *argstostr(int ac, char **av);
 #endif
