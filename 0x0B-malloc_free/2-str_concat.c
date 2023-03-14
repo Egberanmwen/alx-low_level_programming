c6123026964cd7c246d2746d77deab658caed6e7
Egberanmwen <doris_esh@yahoo.com>
2023-03-14 10:12:32 -0700

    "2-str_concat.c"

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <stdio.h>
+
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - Concatenate (combine) two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Pointer to new space in memory, NULL if it fails
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	char *s3, *empt;
+	int i, len1, len2, j;
 
-if (s1 == NULL || s2 == NULL)
-s1 = s2 = "";
-
-for (end1 = 0; end1 <= *s1; end1++)
+empt = "";
+if (s1 == NULL)
+s1 = empt;
+if (s2 == NULL)
+s2 = empt;
+i = len1 = len2 = 0;
+while (s1[len1] != '\0')
 {
+len1++;
 }
-
-for (end2 = 0; end2 <= *s2; end2++)
+while (s2[len2] != '\0')
 {
+len2++;
 }
-
-array = malloc(sizeof(char) * (end1 + end2 + 1));
-
-if (array == NULL)
+len2++;
+s3 = malloc((len1 + len2) *sizeof(*s1));
+if (s3 == NULL)
 return (NULL);
-
-while (*s1)
+i = 0;
+while (i < len1)
 {
-array[i] = *s1;
+s3[i] = s1[i];
 i++;
-s1++;
 }
-
-while (*s2)
+j = 0;
+while (j <= len2)
 {
-array[i] = *s2;
+s3[i] = s2[j];
 i++;
-s2++;
+j++;
 }
-return (array);
+return (s3);
 }
