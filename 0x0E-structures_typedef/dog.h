05b598b6fc89be738c3bb03ddb1f35a41bad7059
Egberanmwen <doris_esh@yahoo.com>
2023-03-20 09:01:51 -0700

    0.Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Desc: header file
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+#endif
