589eea37ccf7940b2c3edc41cf11609f51d7250e
Egberanmwen <doris_esh@yahoo.com>
2023-03-30 06:55:23 -0700

    0.Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t count;
 if (h == NULL)
 return (0);
-n = 1;
+count = 1;
 printf("[%d] %s\n", h->len, h->str);
 if (h->next != NULL)
-n += print_list(h->next);
-return (n);
+count += print_list(h->next);
+return (count);
 }
