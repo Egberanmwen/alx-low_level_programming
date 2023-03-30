093ae7784fc2169173df42bdc6c22b62434d3569
Egberanmwen <doris_esh@yahoo.com>
2023-03-30 10:03:05 -0700

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,20 @@
 #include "lists.h"
-
+#include <stdlib.h>
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to the list
+ *
+ * Return: returns the number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *doris;
+	int count  = 0;
+doris = h;
+while (doris != NULL)
+{
+doris = doris->next;
+count++;
+}
+return (count);
+}
