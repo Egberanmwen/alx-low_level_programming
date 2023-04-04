fa5c959feab4bbee02b22eeb2ec68ba7501f03dc
Egberanmwen <doris_esh@yahoo.com>
2023-04-04 15:56:57 -0700

    4.Free list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,5 +36,7 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint2(listint_t **head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+void free_listint(listint_t *head);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 #endif
