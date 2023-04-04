43d823ef767ca4daab08cbb1c20e12e3c41e1f80
Egberanmwen <doris_esh@yahoo.com>
2023-04-04 07:17:59 -0700

    1.Print list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * listint_len - returns the number of elements in
  * a linked list.
@@ -8,7 +9,7 @@
  */
 size_t listint_len(const listint_t *h)
 {
-size_t nnodes = 0;
+	size_t nnodes = 0;
 while (h != NULL)
 {
 h = h->next;
