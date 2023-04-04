80f1db2f6d5c32b7b5ea9a68352e6dbf74d7fa25
Egberanmwen <doris_esh@yahoo.com>
2023-04-04 07:54:20 -0700

    3.Add node at the end

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,8 +17,22 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 }listint_t;
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
